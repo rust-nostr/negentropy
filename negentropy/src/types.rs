48c2ac1b910a2c3a803a8cae080f2e1cbc9ab346
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-12-14 09:12:03 +0100

    negentropy: fix clippy

--- negentropy/src/types.rs
+++ negentropy/src/types.rs
@@ -211,8 +211,8 @@ impl Accumulator {
         let mut wtr = Vec::with_capacity(ID_SIZE);
 
         for i in 0..4 {
-            let orig = Wrapping(u64::from_le_bytes(p[(i*8)..(i*8 + 8)].try_into()?));
-            let other_v = Wrapping(u64::from_le_bytes(po[(i*8)..(i*8 + 8)].try_into()?));
+            let orig = Wrapping(u64::from_le_bytes(p[(i * 8)..(i * 8 + 8)].try_into()?));
+            let other_v = Wrapping(u64::from_le_bytes(po[(i * 8)..(i * 8 + 8)].try_into()?));
 
             let mut next = orig;
 
